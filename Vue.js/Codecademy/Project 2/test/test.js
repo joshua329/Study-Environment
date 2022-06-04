d41c3a15a2acc00c0aa6ae96443cca3d9b5061c9
Joshua Barnett <barnettjoshua333@gmail.com>
2022-06-04 22:17:06 +1000

    Finished project 2

--- Vue.js/Codecademy/Project 2/test/test.js
+++ Vue.js/Codecademy/Project 2/test/test.js
@@ -1,18 +1,13 @@
-console.log = function() {};
-const { assert } = require('chai');
-const fs = require('fs');
-const Structured = require('structured');
-
-const code = fs.readFileSync('js/app.js', 'utf8');
+console.log = () => {}
+const { expect } = require('chai');
+const rewire = require('rewire');
+const Vue = require('vue');
 
 describe('', function () {
-  it('Did you copy the provided code into **app.js** exactly as it is written?', function () {
-    let structure = function() {
-      const app = new Vue({
-      });
-    };
-
-    let isMatch = Structured.match(code, structure);
-    assert.isOk(isMatch);
-  });
+    it('', function () {
+        let appModule = rewire("../js/app.js");
+        let vueApp = appModule.__get__("app");
+        expect(vueApp).to.exist();
+    });
 });
+       
