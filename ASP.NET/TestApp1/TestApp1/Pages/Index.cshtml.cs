a776fa7336a5459464c3ee9811c94d56d2c8c8e7
Joshua Barnett <barnettjoshua333@gmail.com>
2022-06-28 09:18:31 +1000

    Adding empty ASP.NET Rasor

--- ASP.NET/TestApp1/TestApp1/Pages/Index.cshtml.cs
+++ ASP.NET/TestApp1/TestApp1/Pages/Index.cshtml.cs
@@ -1,21 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace TestApp1.Pages {
-    public class IndexModel : PageModel {
+namespace TestApp1.Pages
+{
+    public class IndexModel : PageModel
+    {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger) {
+        public IndexModel(ILogger<IndexModel> logger)
+        {
             _logger = logger;
         }
 
-        public void OnGet() {
+        public void OnGet()
+        {
 
         }
     }
 }
+
--- ASP.NET/TestApp1/TestApp1/Pages/Privacy.cshtml.cs
+++ ASP.NET/TestApp1/TestApp1/Pages/Privacy.cshtml.cs
@@ -1,20 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace TestApp1.Pages {
-    public class PrivacyModel : PageModel {
+namespace TestApp1.Pages
+{
+    public class PrivacyModel : PageModel
+    {
         private readonly ILogger<PrivacyModel> _logger;
 
-        public PrivacyModel(ILogger<PrivacyModel> logger) {
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
             _logger = logger;
         }
 
-        public void OnGet() {
+        public void OnGet()
+        {
         }
     }
 }
+
